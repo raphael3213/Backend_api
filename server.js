7a4756ee091d9c17b5c89154862cdf67c2a2790d
Glitch (time-stamper12214) <none>
2018-05-22 17:28:36 +0000

    🐗🏟 Checkpoint ./views/index.html:1174538/1046 ./public/client.js:1174538/1190 ./server.js:1174538/1775

--- server.js
+++ server.js
@@ -6,25 +6,4 @@ var express = require('express');
 var app = express();
 var url=require('url');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-var urlobj;
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-  urlobj=url.parse(request.url,true);
-  
-});
-
-
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  console.log(urlobj)
-});
-
-
-
-
+app.listen(
