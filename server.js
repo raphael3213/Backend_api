f017fbd002a03be62a5b40c49611c7ecb2d82437
Glitch (time-stamper12214) <none>
2018-05-22 15:33:27 +0000

    🍲🎐 Checkpoint ./server.js:1174538/172

--- server.js
+++ server.js
@@ -11,17 +11,20 @@ var url=require('url');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
+var urlobj;
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+  urlobj=url.parse(request.url,true);
+  
 });
 
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+  console.log(urlobj)
 });
 
-var query=
+
 
 
